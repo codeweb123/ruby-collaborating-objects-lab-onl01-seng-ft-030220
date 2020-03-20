4108fdeb20c610a59e63823aeaa58817dbb2bbc8
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-20 20:52:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
+
   @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
     @@all << self
   end
 
@@ -12,35 +13,25 @@ class Artist
     @@all
   end
 
+  def songs
+    Song.all.select { | song | song.artist == self}
+  end
+
   def add_song(song)
-    @songs << song
+    song.artist = self
   end
 
   def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    self.find(name) ? self.find(name) : self.new(name)
   end
 
   def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
+    self.all.find { | artist | artist.name == name}
+  end
+
+  def print_songs
+    songs.each do | song |
+      puts song.name
     end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
-  def save
-    @@all << self
-  end
-
-  def print_songs()
-    puts @songs.collect {|x| x.name}
   end
 end
