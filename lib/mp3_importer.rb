5bbeb7dbea0a45e8546749a51436573827dd36c6
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-20 20:54:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,26 @@
 class MP3Importer
 
-  attr_reader :path
+  attr_accessor :path
 
-    def initialize(path)
-      @path = path
-    end
+  def initialize(path)
+    @path = path
+  end
 
-    def files
-      @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect do |file|
+      file.gsub("#{path}/", "")
     end
+  end
 
-    def import
-      self.files.each{|file| Song.new_by_filename(file)}
-    end
+  # set an instance variable that is equal to all the possible paths that music can come in on
+
+  def import
+    files.each{ | file | Song.new_by_filename(file)}
+  end
+
+  # call the files method and loop through each file and create a new song from each filename
+
+
+
+
 end
