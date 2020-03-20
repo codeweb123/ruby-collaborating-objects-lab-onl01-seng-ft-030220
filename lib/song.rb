63cb2218df9bffcfb5b7c6f4dab4560a90d387ba
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-20 20:49:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,24 +11,15 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
-  end
-end
-
-  # def self.new_by_filename(file)
-  #   song_name = file.split(" - ")[1]
-  #   artist = file.split(" - ")[0]
-  #   song = self.new(song_name)
-  #   song.artist_name = artist
-  #   song
-  # end
-
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ") # split it into appropriate pieces
+    new_song = self.new(song) # create new song instance
+    new_song.artist_name = artist # assign it an artist
+    new_song # return the instance
   end
 end
